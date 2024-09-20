52d0ff429e257c1b85ff9e677f81a3e707154aee
igl-myrick <igl.myrick@gmail.com>
2024-09-20 15:59:28 -0700

    add places controller

---
+++ TravelDiary/Controllers/PlacesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using TravelDiary.Models;
+using System.Collections.Generic;
+
+namespace TravelDiary.Controllers
+{
+  public class PlacesController : Controller
+  {
+
+    [HttpGet("/places")]
+    public ActionResult Index()
+    {
+      List<Place> allPlaces = Place.GetAll();
+      return View(allPlaces);
+    }
+
+    [HttpGet("/places/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/places")]
+    public ActionResult Create(string cityName)
+    {
+      Place newPlace = new Place(cityName);
+      return RedirectToAction("Index")
+    }
+    
+  }
+}
