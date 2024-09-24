eaf39e354b312a1d7944e0513aa4ac30ae11c625
igl-myrick <igl.myrick@gmail.com>
2024-09-24 13:50:55 -0700

    update Create route with new Duration property

--- TravelDiary/Controllers/PlacesController.cs
+++ TravelDiary/Controllers/PlacesController.cs
@@ -21,9 +21,9 @@ namespace TravelDiary.Controllers
     }
 
     [HttpPost("/places")]
-    public ActionResult Create(string cityName)
+    public ActionResult Create(string cityName, string duration)
     {
-      Place newPlace = new Place(cityName);
+      Place newPlace = new Place(cityName, duration);
       return RedirectToAction("Index");
     }
 
